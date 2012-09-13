a6f5a14a32d6b3402111ac34b164200e82af80bb
Dan Koester <dan@dan1son.net>
2012-09-13 10:59:46 -0500

    Initial checkin

---
+++ glicko.js
@@ -0,0 +1,416 @@
+//based on http://www.glicko.net/glicko/glicko2.pdf
+//Code by Robert Kohr 2012 (License agreement at the bottom)
+
+
+//wrapper to make it easier to use as a node.js module
+(function(exports){
+	var DEFAULT_RATING = 1500;
+
+    exports.playerInit = function(player){
+	if(!player.rating) {
+	    player.rating = DEFAULT_RATING;
+	    player.rd= 350;
+	    player.vol = 0.06;
+    }
+	player.rating2 = (player.rating - DEFAULT_RATING)/173.7178
+	player.rd2 = player.rd/173.7178
+	return player;
+    }
+
+
+    /*
+Glicko-2 main function (attempts to follow the paper)
+
+Calculate the adjustments to a player after playing against a set of opponents.
+
+Returns an output of an object like so
+{
+  init:{rd, rating, vol}, //initial values before calculating
+  update:{rd, rating, vol}, //new values
+  change:{rd, rating, vol}, //difference between init and update
+} 
+
+rating_period should be the time in seconds in which a player typically plays
+10-15 games. Default is a month. Currently not used in calculations
+
+For system_constants, the paper says pick a number .3 to 1.2.
+Adjust to achieve better predictive accuracy. Default is .75
+(perhaps a function should be written to calculate a good system_constant)
+
+factor is something I added that isn't in the paper. All outputs are multiplied by
+the factor. This is useful if you want to adjust the value of the calculation -
+for example 0.5 to half the weight when you are playing on a team of two players.
+
+*/
+    exports.calc = function(player, opponents, rating_period, system_constant, factor){
+	//set defaults
+	var min = 60; var hour = 60*min; var day = 24*hour;
+	if(!rating_period)
+	    rating_period = day*30;
+	var t = system_constant;
+	if(!factor)
+	    factor = 1;
+
+	//step 1
+	//initialize
+	if(!t)
+	    t = 0.75;
+
+	//p will be the returned object with the updated values for the player
+	var p = {};
+	p.rating = player.rating;
+	p.rd = player.rd;
+	p.vol = player.vol;
+
+	//initialize all players
+	var initialize_set = [];
+	opponents.each(function(opp){
+	    initialize_set.push(opp);
+	});
+	
+
+	initialize_set.push(p);
+	initialize_set.each(function(pl){
+	    //step 2 - set glicko-2 values (and initial values).
+	    exports.playerInit(pl);
+	});
+
+	var out = {update:{}, change:{}, init:{rating:p.rating, rd:p.rd, vol:p.vol}};
+
+	//step 3
+	var g = function(rd){
+	    return 1/(
+		Math.sqrt(
+		    1 + (
+			(3*pow(rd, 2))/
+			    (pow(pi,2))
+		    )
+		)
+	    );
+	};
+
+	//the paper has three values comming in for the two players, this just takes the player objects
+	var E = function(p1, p2){
+	    if(!p1.rd2)
+		throw "Missing player 1";
+	    if(!p2.rd2)
+		throw "Missing player 2"+p2.rd2;
+	    
+	    return 1 / (1 + Math.exp((-g(p2.rd2)*(p1.rating2-p2.rating2))));
+	}
+
+	//iterate over all opponents he played against to calculate variance
+    	var v_sum = 0;
+	opponents.each(function(opp){
+	    var this_v = (pow(g(opp.rd2),2)) * E(p, opp) * (1 - E(p, opp) );
+	    v_sum += this_v;	    
+	});
+	var v = pow(v_sum, -1);
+
+	//step 4
+	var part_delta_sum = 0;
+	opponents.each(function(opp){
+	    var this_delta_part = g(opp.rd2) * (opp.s - E(p, opp) )
+	    part_delta_sum += this_delta_part;
+	});
+
+	
+	//delta is the change in rating
+	var delta = v * part_delta_sum;
+	
+	var	makef = function(delta, v, a){
+		  return function(x){
+		    return Math.exp(x) * (Math.pow(delta, 2) - Math.pow(p.rd2, 2) - v - Math.exp(x)) / (2*Math.pow(Math.pow(p.rd2, 2) + v + Math.exp(x), 2)) - (x - a) / Math.pow(t, 2);
+		  };
+		};
+
+			
+	// Step 5 (ripped and modded from glicko2js, this one was broken somehow
+		var a = Math.log(Math.pow(p.vol, 2));
+		var f = makef(delta, v, a);
+		var epsilon = 0.000001;
+		var A = a;
+		var B, k;
+
+		if (Math.pow(delta, 2) > Math.pow(p.rd2, 2) + v) {
+			B = Math.log(Math.pow(delta, 2) - Math.pow(p.rd2, 2) - v);
+		} else {
+			k = 1;
+			while (f(a - k * t) < 0) {
+				k = k + 1;
+			}
+			B = a - k * t;
+		}
+
+		var fA = f(A);
+		var fB = f(B);
+		var C, fC;
+		while (Math.abs(B - A) > epsilon) {
+			C = A + (A - B) * fA / (fB - fA);
+			fC = f(C);
+			if (fC * fB < 0) {
+				A = B;
+				fA = fB;
+			} else {
+				fA = fA / 2;
+			}
+			B = C;
+			fB = fC;
+		}
+		var vol_prime = Math.exp(A / 2);
+
+		// Original step 5
+// //5.1
+// var a = ln(pow(p.vol,2));
+// var f = function(x){
+//	    return (
+//		(Math.exp(x)*(pow(delta,2)-pow(p.rd2,2)-v-Math.exp(x))) / 
+//		    (
+//			2* pow( 
+//			    (pow(p.rd2,2) + v + Math.exp(x) ),
+//			    2)
+//		    )
+//	    ) - (
+//		(x - a)/(pow(t,2))
+//	    )
+//	}
+//	var e = 0.000001;//convergence tolerance
+//
+//	//5.2
+//	var A = a;
+//	if( pow(delta,2) > (pow(p.rd2,2) + v) ){
+//	    var B = ln(pow(delta,2) - pow(p.rd2,2) - v);
+//	}else{
+//	    var k = 1;
+//	    while(f(a - (k*Math.sqrt(Math.pow(t,2)))) < 0){
+//		k = k+1;
+//	    }
+//	    var B = a - k * Math.sqrt(Math.pow(t,2));
+//	}
+//
+//	//5.3
+//	fa = f(A);
+//	fb = f(B);
+//
+//	//5.4
+//	if (player.name == 'warwickftw') 
+//    	console.log("uhhh: " + (B - A));
+//	while((Math.abs(B-A) > e)){
+//		if (player.name == 'warwickftw') 
+//	    	console.log("uhhh: " + (B - A));
+//	    var C = A+(A-B)*fa/(fb-fa);
+//	    fc = f(C);
+//	    if((fc*fb)<0){
+//		A = B;
+//		fa = fb;
+//	    }else{
+//		fa = fa/2;
+//	    }
+//	    B = C;
+//	    fb = fc;
+//	}
+//	if (player.name == 'warwickftw') 
+//    	console.log("uhhh: " + (B - A));
+//	var vol_prime = Math.exp(A/2);
+
+	//Step 6
+	var rd2_star = Math.sqrt(Math.pow(p.rd2,2) + Math.pow(vol_prime,2));
+	//Step 7
+	var rd2_prime = 1/(
+	    Math.sqrt( 
+		( (1/(Math.pow(rd2_star, 2))) + 1/v )
+	    ));
+
+	var rating2_prime_sum = 0;
+	opponents.each(function(opp){
+	    var sum_el = g(opp.rd2)*(opp.s - E(p, opp));
+	    rating2_prime_sum += sum_el;
+	});
+	var rating2_prime = p.rating2 + pow(rd2_prime,2) * rating2_prime_sum;
+
+	//lets track changes to player
+	if (opponents.length != 0 ) {
+		p.rating2 = rating2_prime;
+		p.rd2 = rd2_prime;
+		p.vol = vol_prime;
+	} else {
+		p.rd2 = Math.sqrt(Math.pow(p.rd2,2) + Math.pow(p.vol,2));;
+	}
+
+	//step 8 convert back to original scale
+	p.rating = p.rating2 * 173.7178 + DEFAULT_RATING;
+	p.rd = p.rd2 * 173.7171;
+
+	out.change.rating = (p.rating - out.init.rating)*factor;
+	out.change.rd = (p.rd - out.init.rd)*factor;
+	out.change.vol = (p.vol - out.init.vol)*factor;
+
+	out.update.rating = out.change.rating + out.init.rating;
+	out.update.rd = out.change.rd + out.init.rd;
+	out.update.vol = out.change.vol + out.init.vol;	
+	out.timestamp = (new Date()).getTime() / 1000;//in seconds from unix epoch
+	return out;
+    }
+
+
+
+
+/* 
+Team match calculator (and updater of players in the match)
+
+Example:
+teams = 
+[
+  {
+   rank:1,
+   name:'Blue Jays', 
+   players:
+    [
+      {name:'bob', rating:3300, rd:4},
+      {name:'steve', rating:3300, rd:4},
+    ]
+  },
+  {
+    rank:2,
+    name:"Robins',
+    players:
+    [ ...]
+   }
+];
+
+This function works by calculating what the change would be for each player against all the players
+on the opposing team, averaging them, and then dividing by the number of players on that player's team. 
+
+returns {
+         teams:
+          [same as input, but with all new values applied to players], 
+         players:
+          [array of just updated players (no team structure)]
+        }
+*/
+    exports.teamMatch = function(teams, rating_period, system_constant){
+	var players = [];
+
+	teams.each(function(team, t1){
+	    team.players.each(function(p){
+		players.push(p);
+		var num_players = team.players.length;
+
+		//have the player play against all other teams
+		var opponents = [];
+		//round up opponents
+		teams.each(function(team2, t2){
+		    if(t2==t1)
+			return; //same team as player p
+		    if( (!team2.players) || (!(team2.players.length>0)) )
+			return;//no players on team
+
+
+		    var team_as_player = {rd:0,rating:0,vol:0.06};
+		    team2.players.each(function(p2){
+			exports.playerInit(p2);
+			team_as_player.each(function(val, key){
+			    team_as_player[key] += p2[key];
+			});
+		    });
+		    //team as player has the sum of all players, now 
+		    //need to devide to average it all out.
+		    team_as_player.each(function(val, key){
+			team_as_player[key] = val/team2.players.length;
+		    });
+		    team_as_player.s = exports.s_from_rank(team, team2);
+		    opponents.push(team_as_player);
+		});
+		var result = exports.calc(p, opponents, null, null, 1/num_players);
+		if(!p.result_history) 
+		    p.result_history = [];
+		p.result_history.push(result);
+		p.last_result = result;
+	    });
+	});
+
+	players.each(function(p){
+	    var update = p.last_result.update;
+	    update.each(function(val, key){
+		p[key] = val;
+	    });
+	});
+	return {teams:teams, players:players};
+    }
+    exports.s_from_rank = function(p, p2){
+	if(p2.rank < p.rank)
+	    return 0;
+	if(p2.rank == p.rank)
+	    return .5;
+	return 1;
+    }
+
+
+
+
+
+//   Helper Functions ///
+    var pow = Math.pow;
+    var pi = Math.PI;
+
+    function ln(val){
+	return Math.log(val) / Math.LOG10E;
+    }
+    var debug = function(v){
+	console.log(JSON.stringify(v));
+    }
+
+
+    Array.prototype.each = function(callback){
+	var arr = this;
+	for(var i = 0; i<arr.length; i++){
+	    var el = arr[i];
+	    if(typeof(el)!='function')
+		callback(el, i);
+	}
+    }
+
+    Object.prototype.each = function(callback){
+	var obj = this;
+	for(var key in obj){
+	    var el = obj[key];
+	    if(typeof(el)!='function')
+		callback(el, key);
+	}
+    }
+
+
+
+})(typeof exports === 'undefined'? this['glicko']={}: exports);//end module
+
+
+/*
+License for this file (and this file only).
+
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+    In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+
+*/
---
+++ mcp-glicko2.js
@@ -0,0 +1,321 @@
+// ---- CONFIG OPTIONS
+
+// How many fights to look calc at a time for initial load.
+var CHUNK_SIZE = 150;
+
+// how often to check for updates 14400000 milliseconds  = 4 hours
+var UPDATE_INTERVAL = 14400000;
+
+// Path goodies
+var FIGHT_JSON_HOST = 'nerd.nu';
+var FIGHT_JSON_PATH = '/survival/survivalstats.json';
+
+
+var express = require('express');
+var http = require('http');
+var app = express();
+var timers = require('timers');
+
+var glicko = require('./glicko.js');
+
+// Objects to store all player data playerMap.playername
+var playerMap = {};
+var playerMapArmor = {};
+var playerMapNoArmor = {};
+
+// lastId of calculated data
+var lastId = 0;
+
+function doMaths() {
+	http.request({host: FIGHT_JSON_HOST, path: FIGHT_JSON_PATH},
+		function (response) {
+			var str = '';
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+
+			response.on('end', function () {
+				if (str) {
+					var json = JSON.parse(str);
+					json.sort(function(a,b) {return a.id - b.id });
+					var calcCount = 0;
+					var i2 = 0;
+					var chunk = [];
+					for (i = 0; i < json.length; i++) {
+						if (lastId < json[i].id) {
+							// Keeps track of the total;
+							calcCount++;
+							// Make sure we don't do the same ones twice. 
+							lastId = json[i].id;
+							chunk[i2++] = json[i];
+							
+							// Chunk it
+							if (i2 >= CHUNK_SIZE || i == json.length - 1) {
+								calcBattles(chunk);
+								chunk = [];
+								i2 = 0;
+							}
+						}
+					}
+					console.log('Successful: ' + calcCount + ' - Now Sorting and Ranking');
+                        
+                    // Sort all of the lists
+                    var allPlayers = getSortedPlayers(playerMap);
+                    var allPlayersArmor = getSortedPlayers(playerMapArmor);
+                    var allPlayersUnArmored = getSortedPlayers(playerMapNoArmor);
+                        
+                    // Update the variables in the main playerMap
+                    for (i=0; i < allPlayers.length; i++) {
+                        var player = playerMap[allPlayers[i].name];
+                        if (allPlayers[i].name) {
+                            player.rank = i;
+                            playerMap[allPlayers[i].name] = player;
+                        }
+                    }
+                    for (i=0; i < allPlayersArmor.length; i++) {
+                        var tempPlayer = allPlayersArmor[i];
+                        var player = playerMap[tempPlayer.name];
+                        if (player) {
+                            player.rankArmor = i;
+                            player.ratingArmor = tempPlayer.rating;
+                            player.rdArmor = tempPlayer.rd;
+                            player.volArmor = tempPlayer.vol;
+                            playerMap[tempPlayer.name] = player;
+                        }
+                    }
+                    for (i=0; i < allPlayersUnArmored.length; i++) {
+                        var tempPlayer = allPlayersUnArmored[i];
+                        var player = playerMap[tempPlayer.name];
+                        if (player) {
+                            player.rankNoArmor = i;
+                            player.ratingNoArmor = tempPlayer.rating;
+                            player.rdNoArmor = tempPlayer.rd;
+                            player.volNoArmor = tempPlayer.vol;
+                            playerMap[tempPlayer.name] = player;
+                        }
+                    }
+                    console.log('Successful: ' + calcCount + ' - Done');
+				}
+			});
+		}).end();
+}
+
+function calcBattles(chunkedData) {
+	// Go through fights, building player vs opponent maps
+	var dupeCheck = [];
+	var dupeCheckArmor = [];
+	var dupeCheckNoArmor = [];
+	
+	for (vi = 0; vi < chunkedData.length; vi++) {
+		var fight = chunkedData[vi];
+		var killer = fight.killer_name;
+
+		// We need to use both the "killer" and "killee"  killer attr used for both.
+		while (killer) {
+			if (!dupeCheck[killer]) {
+				opponents = [];
+				opponentsArmor = [];
+				opponentsNoArmor = [];
+				// Go through fights again, grab all opponents for 'killer'
+				for (vi2 = vi; vi2 < chunkedData.length; vi2++) {
+					// Check if killer is involved
+					var altercation = chunkedData[vi2];
+                    
+					var weapon = altercation.killer_item;
+					// Only worry about certain weapons.
+					if (weapon.indexOf('DIAMOND') == -1 && weapon.indexOf('IRON') == -1 && weapon.indexOf('BOW') == -1 && weapon.indexOf('POTION') == -1) {
+						continue;
+					}
+					// Ignore suicides 
+					if (altercation.killer_name == altercation.player_name) {
+						continue;
+					}
+					var isArmor = altercation.armor_kill;
+					// Handle the combined stats first
+					if (altercation.killer_name == killer) {
+						dupeCheck[killer] = true;
+						updateOpponent(altercation.player_name, playerMap, opponents, 1);
+						// Split stats
+						if (isArmor) {
+							dupeCheckArmor[killer] = true;
+							updateOpponent(altercation.player_name, playerMapArmor, opponentsArmor, 1);
+						} else {
+							dupeCheckNoArmor[killer] = true;
+							updateOpponent(altercation.player_name, playerMapNoArmor, opponentsNoArmor, 1);
+						}
+					}
+					// Handle losses
+					if (altercation.player_name == killer) {
+						dupeCheck[killer] = true;
+						updateOpponent(altercation.killer_name, playerMap, opponents, 0);
+						if (isArmor) {
+							dupeCheckArmor[killer] = true;
+							updateOpponent(altercation.killer_name, playerMapArmor, opponentsArmor, 0);
+						} else {
+							dupeCheckNoArmor[killer] = true;
+							updateOpponent(altercation.killer_name,	playerMapNoArmor, opponentsNoArmor, 0);
+						}
+					}
+				}
+				// Do the actual glicko computations and update the players
+				if (dupeCheck[killer]) {
+					updatePlayer(killer, playerMap, opponents);
+				}
+				if (dupeCheckArmor[killer]) {
+					updatePlayer(killer, playerMapArmor, opponentsArmor);
+				}
+				if (dupeCheckNoArmor[killer]) {
+					updatePlayer(killer, playerMapNoArmor, opponentsNoArmor);
+				}
+			}
+			
+			// Do the killed player if we haven't already
+			if (killer == fight.player_name) {
+				killer = null;
+			} else {
+				killer = fight.player_name;
+			}
+		}
+	}
+	for (playerName in playerMap) {
+		if (!dupeCheck[playerName]) {
+			updatePlayerNoKills(playerName, playerMap);
+		}
+	}
+	for (playerName in playerMapArmor) {
+		if (!dupeCheckArmor[playerName]) {
+			updatePlayerNoKills(playerName, playerMapArmor);
+		}
+	}
+	for (playerName in playerMapNoArmor) {
+		if (!dupeCheckNoArmor[playerName]) {
+			updatePlayerNoKills(playerName, playerMapNoArmor);
+		}
+	}
+}
+
+function updateOpponent(opponentName, mapOfPlayers, thisOpponents, score) {
+	var opponent = {};
+	opponent.temp_name = opponentName;
+	opponent.s = score;
+	// Pull the opponent out of the map if we have it and use those stats.
+	if (mapOfPlayers[opponentName]) {
+		var realOpponent = mapOfPlayers[opponentName];
+		opponent.rating = realOpponent.rating;
+		opponent.rd = realOpponent.rd;
+		opponent.vol = realOpponent.vol;
+	}
+	thisOpponents[thisOpponents.length] = opponent;
+	return thisOpponents;
+}
+
+function updatePlayer(playerName, mapOfPlayers, thisOpponents) {
+	player = {};
+	// Pull from the map if we have it, otherwise glicko.calc fills in the defaults 
+	if (mapOfPlayers[playerName]) {
+		player = mapOfPlayers[playerName];
+	} else {
+		player.name = playerName;
+	}
+	
+	// Calc it!
+	var update = glicko.calc(player, thisOpponents, null, .75, 1);
+	
+	// Update it
+	player.rating = update.update.rating;
+	player.rd = update.update.rd;
+	player.vol = update.update.vol;
+	mapOfPlayers[playerName] = player;
+}
+
+function updatePlayerNoKills(playerName, mapOfPlayers) {
+	// Player must be in map, run it throught the calc to update the RD. 
+	player = mapOfPlayers[playerName];
+	var update = glicko.calc(player, [], null, .75, 1);
+	player.rating = update.update.rating;
+	player.rd = update.update.rd;
+	player.vol = update.update.vol;
+	mapOfPlayers[playerName] = player;
+}
+
+function getSortedPlayers(mapOfPlayers) {
+	var allPlayers = [];
+	var xi = 0;
+	for (player in mapOfPlayers) {
+		playerObj = mapOfPlayers[player];
+        
+        // In loops are dumb on large objects
+        if (playerObj.name == "") {
+            continue;
+        }
+        
+		var newPlayer = {};
+		newPlayer.rating = playerObj.rating;
+		newPlayer.name = playerObj.name;
+		newPlayer.rd = playerObj.rd;
+		newPlayer.vol = playerObj.vol;
+		allPlayers[xi] = newPlayer;
+        xi++;
+	}
+
+	allPlayers.sort(function(a, b) { if (a.rating && b.rating) { return b.rating - a.rating } else return -1});
+	return allPlayers;
+}
+
+function getFinalArray(mapOfPlayers) {
+    var allPlayers = [];
+	var xi = 0;
+	for (player in mapOfPlayers) {
+		playerObj = mapOfPlayers[player];
+        
+        // In loops are dumb on large objects
+        if (playerObj.name == "") {
+            continue;
+        }
+		var newPlayer = {};
+		newPlayer.rating = playerObj.rating;
+		newPlayer.name = playerObj.name;
+		newPlayer.rank = playerObj.rank;
+		newPlayer.rankArmor = playerObj.rankArmor;
+        newPlayer.rankNoArmor = playerObj.rankNoArmor;
+		allPlayers[xi] = newPlayer;
+        xi++;
+	}
+    
+	allPlayers.sort(function(a, b) { if (a.rating && b.rating) { return b.rating - a.rating } else return -1});
+	return allPlayers;
+}
+
+// Do initial load and set a timer
+doMaths();
+timers.setInterval(doMaths, UPDATE_INTERVAL);
+
+app.options('/', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.end('');
+});
+
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.send(getFinalArray(playerMap));
+	console.log('Connection: ' + req);
+});
+
+app.options('/player', function(req, res) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.end('');
+            });
+
+app.get('/player', function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.send(playerMap[req.query['name']]);
+        console.log('Connection: ' + req);
+        });
+
+
+app.listen(8282);
+console.log('Server running at http://127.0.0.1:8282/');
